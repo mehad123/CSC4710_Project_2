c161fed343b3c3ba6dedf0cb3c5bd48acdfde6c2
saleh-programs <muradsaleh2022@gmail.com>
2025-12-09 13:11:11 -0500

    Set up all network requests to fill containers

--- Frontend/anna/annaDashboard.js
+++ Frontend/anna/annaDashboard.js
@@ -1,27 +1,77 @@
 const backendURL = "http://localhost:5050";
 
+
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const response = await fetch(backendURL + "/users");
-    const clients = await response.json()
-    loadTable(clients);
+    /*
+    This topmost "get all users" can easily be optimized to only send 
+    a fixed amount at a time, but it's almost never needed until we have
+    hundreds of clients
+    */
+    fetch(backendURL + "/users")
+    .then(res=> res.json())
+    .then(all => loadAll(all));
+
+    fetch(backendURL + "/users/frequents")
+    .then(res=> res.json())
+    .then(freqeunts => loadFrequents(freqeunts));
+
+    fetch(backendURL + "/users/uncommitted")
+    .then(res=> res.json())
+    .then(uncommitted => loadUncommited(uncommitted));
+
+    fetch(backendURL + "/users/prospectives")
+    .then(res=> res.json())
+    .then(prospectives => loadProspectives(prospectives));
+
+    fetch(backendURL + "/users/bad")
+    .then(res=> res.json())
+    .then(bad => loadBad(bad));
+
+    fetch(backendURL + "/users/good")
+    .then(res=> res.json())
+    .then(good => loadGood(good));
+
+    fetch(backendURL + "/service-requests/accepted")
+    .then(res=> res.json())
+    .then(accepted => loadAccepted(accepted));
+
+    fetch(backendURL + "/service-requests/largest")
+    .then(res=> res.json())
+    .then(larges => loadLargest(larges));
+
+    fetch(backendURL + "/bills/overdue")
+    .then(res=> res.json())
+    .then(overdue => loadOverdue(overdue));
+
 });
 
-function loadTable(queries){
+function loadAll(items){}
+function loadFrequents(ites){}
+function loadUncommited(items){}
+function loadProspectives(items){}
+function loadBad(items){}
+function loadGood(items){}
+function loadAccepted(items){}
+function loadLargest(items){}
+function loadOverdue(items){}
+
+function loadClients(queries){
     const clients = document.getElementById("client-list");
     let content = "";
     queries.forEach(c => {
-        c["requestIDs"] = ["123"]
         content += `
         <li>
             <section class='client'>
+                Client Name: ${c["name"]}
+                <br/>
+                Client Service Requests: 
                 <ul>
-                    <li>Name: ${c["firstname"]} ${c["lastname"]}</li>
-                    <li>Client ID: ${c["clientID"]}</li>
-                    <li>Service Requests:<ul>
-                    ${c["requestIDs"].map(id=>
-                        `<li><a href="annaSR.html?requestID=${id}">${id}</a></li>`
-                    ).join("")}
-                    </ul></li>
+                    ${
+                        c["SRlist"].map(elem=>{
+                            return `<li><a href='annaSR.html?SR=${elem}'>${elem}</a></li>`
+                        }).join("")
+                    }
                 </ul>
             </section>
         </li>`;
